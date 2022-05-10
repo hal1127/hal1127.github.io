1f762855667987e487cf7e487d1d898ff1fb0374
hal1127 <harukium@gmail.com>
2022-05-10 17:40:05 +0900

    Excelからのコピペに対応しました

--- csv-to-textbl/src/main.js
+++ csv-to-textbl/src/main.js
@@ -1,6 +1,20 @@
 import $ from 'jquery'
 import ClipboardJS from 'clipboard'
 
+function convertToLatexTable(val) {
+  if (val.trim() === '') return ''
+  const csvArr = val.trim().split('\n')
+  const colLength = (csvArr[0].match(/[^\\]\,/g) || []).length
+
+  const divideChar = (mode === 'csv' ? ',' : '\t')
+  const tblArr = csvArr.map(x => x.replaceAll(divideChar, ' & '))
+  const tbl = tblArr.map(x => '    '+x+' \\\\\\hline\n').join('')
+
+  let latexTbl = latexTblHead.replace('$colstyle$', '|c'.repeat(colLength+1)+'|') + tbl + latexTblFoot
+
+  return latexTbl
+}
+
 const latexTblHead = `\\begin{table}[tb]\\center
   \\caption{表タイトル}
   \\begin{tabular}{$colstyle$}\\hline
@@ -10,13 +24,22 @@ const latexTblFoot = `  \\end{tabular}
 \\end{table}
 `
 
-$('#csv').on('keyup', function() {
-  const csvArr = $('#csv').val().trim().split('\n')
-  const colLength = (csvArr[0].match(/[^\\]\,/g) || []).length
-  const tblArr = csvArr.map(x => x.replaceAll(',', ' & '))
-  const tbl = tblArr.map(x => '    '+x+' \\\\\\hline\n').join('')
+let mode = 'csv'
+$('#mode-select').on('change', function() {
+  mode = $(this).val()
 
-  let latexTbl = latexTblHead.replace('$colstyle$', '|c'.repeat(colLength+1)+'|') + tbl + latexTblFoot
+  if (mode === 'Excel') $('#notion').text('*コピペ専用')
+  else $('#notion').text('')
+
+  $('#mode').text(mode)
+  $('title').text(`${mode}→latexのtable`)
+
+  const latexTbl = convertToLatexTable($('#csv').val())
+  $('#tbl').val(latexTbl)
+})
+
+$('#csv').on('keyup', function() {
+  const latexTbl = convertToLatexTable($(this).val())
   $('#tbl').val(latexTbl)
 })
 
@@ -28,4 +51,3 @@ btnClipboard.on('success', function() {
 btnClipboard.on('error', function() {
   alert('コピーに失敗しました...')
 })
-
